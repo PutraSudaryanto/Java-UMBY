05c48f391efae52abdd6f4632da2e27f52837a06
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-01-16 08:49:53 +0700

    p1 and p2: fixed

---
+++ p1,p2/PutraSudaryanto.java
@@ -0,0 +1,12 @@
+/*
+ * Putra Sudaryanto
+ * 12111075
+ */
+
+public class PutraSudaryanto {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Dwi Putra Sudaryanto,\nNIM: 12111075");
+    }
+
+}
